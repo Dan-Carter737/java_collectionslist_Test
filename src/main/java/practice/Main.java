b675ddc75a52440186aa66ecf63846e714edd974
Денис Коротков <Dan-Carter@mail.ru>
2024-11-25 20:17:05 +0400

    First testing

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,68 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
-    private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        TodoList todoList = new TodoList();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в список дел.Введите команду");
+
+        while (true){
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ",3);
+
+            String command = parts[0].toUpperCase();
+
+            switch (command){
+                case "LIST":
+                   todoList.list();
+                    break;
+                case "ADD":
+                    if (parts.length>1) {
+                        try {
+                            int index = Integer.parseInt(parts[1])-1;
+                            String todo = parts.length > 2 ? parts[2]:"";
+                            todoList.add(index, todo);
+                        }catch (NumberFormatException e){
+                            String todo = parts.length>1? input.substring(4):"";
+                            todoList.add(todo);
+                        }
+                    }
+                    break;
+
+                case "EDIT":
+                    if (parts.length==3) {
+                        try {
+                            int index = Integer.parseInt(parts[1])-1;
+                            String newTodo = parts[2];
+                            todoList.edit(index,newTodo);
+                        }catch (NumberFormatException e){
+                            System.out.println("Некорректный номер для команды EDIT");
+                        }
+                    }else {
+                        System.out.println("Используйте команду EDIT <номер> <новое дело>");
+                    }
+                    break;
+                case "DELETE":
+                    if (parts.length==2){
+                        try {
+                            int index = Integer.parseInt(parts[1])-1;
+                            todoList.delete(index);
+                        }catch (NumberFormatException e){
+                            System.out.println("Некорректный номер для команды DELETE");
+                        }
+                    }else {
+                        System.out.println("Используйте команду DELETE <номер>");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Неизвестная команда.Доступные команды:LIST,ADD,EDIT,DELETE");
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -1,31 +1,55 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TodoList {
 
-    public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+    private List<String> todos;
+
+    public void ToDoList() {
+        this.todos = new ArrayList<>();
     }
 
-    public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+    public TodoList() {
+        this.todos = todos;
     }
 
-    public void edit(int index, String todo) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+    public List<String> getTodos(){
+        return todos;
     }
 
-    public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+    public void add(String todo){
+        todos.add(todo);
     }
 
-    public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        public void edit(int index,String newTodo){
+        if (index >= 0 && index <todos.size()) {
+            todos.set(index,newTodo);
+        }
+    }
+    public void add(int index,String todo){
+        if (index <0 || index>todos.size()) {
+            todos.add(todo);
+        }else{
+            todos.add(index,todo);
+        }
+    }
+
+    public void delete(int index){
+        if (index >=0&&index <todos.size()) {
+            todos.remove(index);
+        }
+    }
+
+    public void list(){
+        if (todos.isEmpty()) {
+            System.out.println("Список дел пустой");
+        }else {
+            for (int i = 0; i < todos.size(); i++) {
+                System.out.println((i+1)+"."+todos.get(i));
+            }
+        }
     }
 
 }
